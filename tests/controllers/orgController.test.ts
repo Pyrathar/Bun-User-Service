52907994871995b12169f10895799d07a5372a9f
Pyrathar <paulobotelhoazores@protonmail.com>
2023-09-20 21:20:44 +0200

    fix: add fix to tets

--- tests/controllers/orgController.test.ts
+++ tests/controllers/orgController.test.ts
@@ -5,31 +5,31 @@ beforeEach(async () => {
     await orgController.deleteAllOrgs()
   });
 
-describe("org Controller", () => {
+describe("Organization Controller", () => {
 
   let sampleorg = {
     name: 'Capsule Corporation',
   }
 
-  test("It creates a org", async () => {
+  test("It creates a organization", async () => {
     let {id, ...org} = await orgController.upsertOrg(sampleorg)
     expect(org).toEqual(sampleorg);
   });
 
-  test("It finds an org by id", async () => {
+  test("It finds an organization by id", async () => {
     let {id} = await orgController.upsertOrg(sampleorg)
     let org = await orgController.getOrgById(id);
     expect(org.name).toEqual(sampleorg.name);
   });
 
-  test("It finds and updates the org when given an id", async () => {
+  test("It finds and updates the organization when given an id", async () => {
     let org = await orgController.upsertOrg(sampleorg)
     org.name="Red Ribbon"
     org = await orgController.upsertOrg(org)
     expect(org.name).toEqual("Red Ribbon");
   });
 
-  test("It deletes an org", async () => {
+  test("It deletes an organization", async () => {
     let {id} = await orgController.upsertOrg(sampleorg)
     await orgController.deleteOrg(id)
     let org = await orgController.getOrgById(id);
